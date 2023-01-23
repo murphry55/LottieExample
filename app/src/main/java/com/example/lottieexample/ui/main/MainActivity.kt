9a835386d3f37070951363f25dfc32e298c44ae0
murphry55 <93278852+murphry55@users.noreply.github.com>
2023-01-23 20:15:21 +0300

    Добавлен проект

---
+++ app/src/main/java/com/example/lottieexample/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lottieexample.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.example.lottieexample.R
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/lottieexample/ui/start/StartFragment.kt
@@ -0,0 +1,6 @@
+package com.example.lottieexample.ui.start
+
+import androidx.fragment.app.Fragment
+import com.example.lottieexample.R
+
+class StartFragment : Fragment(R.layout.fragment_start)
